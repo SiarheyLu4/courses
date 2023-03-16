a7f1e6131fc4d91e6b737d28b78848a132f215dc
Siarhei Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2023-03-16 21:28:30 +0200

    add Album

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
+import Album from "./pages/Album/Album";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Album/>
   );
 };
